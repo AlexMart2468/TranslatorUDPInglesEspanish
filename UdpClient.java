1413d13b6b4736716e12aaf732cf938e7ddd6619
AlexMart2468 <102619183+AlexMart2468@users.noreply.github.com>
2023-07-07 04:43:30 +0000

    Version 2, reorganiza el codigo

--- UdpClient.java
+++ UdpClient.java
@@ -4,54 +4,33 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
 
-
 public class UdpClient {
-    // Definir el destino para enviar el datagrama
-    public static final int DEST_PORT = 30000;
-
+    public static final int DEST_PORT = 30001;
     public static final String DEST_IP = "127.0.0.1";
-    // Define el tamaño máximo de cada datagrama como 4K
     private static final int DATA_LEN = 4096;
-    // Definir la matriz de bytes para recibir datos de la red.
     byte[] inBuff = new byte[DATA_LEN];
-
-
-    // Cree un objeto DatagramPacket que acepte datos de respuesta
     private DatagramPacket packet_in = new DatagramPacket(inBuff, inBuff.length);
-
-    // Definir un objeto DatagramPacket para enviar
     private DatagramPacket packet_out = null;
-
-
 
     public void start() throws IOException {
         try (DatagramSocket socket = new DatagramSocket()) {
-            // Inicializa DatagramSocket para enviar
             InetAddress ip = InetAddress.getByName(DEST_IP);
             packet_out = new DatagramPacket(new byte[0], 0, ip, DEST_PORT);
-            try (// Crear secuencia de entrada de teclado
-            Scanner sc = new Scanner(System.in)) {
-                System.out.println("Por favor ingrese los datos");
-                // Constantemente lee la entrada del teclado
-                String key =null;
-                // Matriz de bytes correspondiente a los caracteres de entrada del teclado
+            try (Scanner sc = new Scanner(System.in)) {
+                System.out.println("Por favor ingrese los datos:");
+                String key = null;
                 byte[] keyBuff = null;
                 while (sc.hasNextLine()) {
                     key = sc.nextLine();
                     if ("exit".equals(key)) {
                         break;
                     }
-                    // cadena de entrada → matriz de bytes
-                    //TO DO
-                    keyBuff= key.getBytes("UTF-8");
-                    //keyBuff = key.getBytes();
-                    // Establecer los datos de bytes en el DatagramPacket para enviar
+                    keyBuff = key.getBytes("UTF-8"); // Especifica la codificación como UTF-8
                     packet_out.setData(keyBuff);
-                    // enviar datagrama
                     socket.send(packet_out);
-                    // Lee los datos en el Socket, y los datos leídos se colocan en la matriz de bytes encapsulada por inPacket.
                     socket.receive(packet_in);
-                    System.out.println(new String(inBuff, 0, packet_in.getLength()));
+                    String response = new String(inBuff, 0, packet_in.getLength());
+                    System.out.println("Respuesta del servidor: " + response);
                     System.out.println("Por favor ingrese los datos:");
                 }
             }
--- UdpDictServer.java
+++ UdpDictServer.java
@@ -1,80 +1,48 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketAddress;
+import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class UdpDictServer {
+    public static final int PORT = 30001;
+    public static final int BUFFER_SIZE = 4096;
+    private static final Map<String, String> wordMap = new HashMap<>();
 
-
-    public static void main(String[] args) throws Exception {
-        new UdpDictServer().start();
+    static {
+        wordMap.put("dog", "perro");
+        wordMap.put("cat", "gato");
+        wordMap.put("fish", "pez");
+        wordMap.put("bird", "pájaro");
+        wordMap.put("pig", "cerdo");
     }
 
-    // Definir el número de puerto udp
-    public static final int PORT = 30000;
-    // Cada datagrama es de hasta 4K
-    private static final int DATA_LEN = 4096;
-    // Matriz de bytes para recibir datos de la red
-    byte[] buff_in = new byte[DATA_LEN];
-    // Cree un objeto DatagramPacket listo para aceptar datos con la matriz de bytes especificada
-    private DatagramPacket packet_in = new DatagramPacket(buff_in, buff_in.length);
+    public static void main(String[] args) {
+        try (DatagramSocket socket = new DatagramSocket(PORT)) {
+            System.out.println("El servidor inglés-español ha comenzado...");
 
-    // Definir un objeto DatagramPacket para enviar
-    private DatagramPacket packet_out;
+            while (true) {
+                DatagramPacket requestPacket = new DatagramPacket(new byte[BUFFER_SIZE], BUFFER_SIZE);
+                socket.receive(requestPacket);
 
-    public void start() throws Exception {
-        try(DatagramSocket socket = new DatagramSocket(PORT)){
-            String key = null;  // Palabras inglesas ingresadas por el cliente
-            String value=null;  // Contenido español traducido por el servidor
-            SocketAddress address=null; // Obtener el objeto del cliente y escribir datos a la otra parte a través del objeto, de lo contrario no hay destino
-            byte[] reviceData=null; // Datos devueltos al cliente
-            System.out.println("El servidor inglés-Español ha comenzado ...");
-            while (true){
-                // Lee los datos en el socket y luego encapsula los datos en packet_in
-                socket.receive(packet_in);
-                // Obtener los datos ingresados ​​por el cliente
-                buff_in=packet_in.getData();
-                // Para convertir la matriz de bytes en una cadena, debe eliminar los espacios finales
-                key=new String(buff_in,0,buff_in.length).trim();
+                String request = new String(requestPacket.getData(), 0, requestPacket.getLength()).trim();
+                InetAddress clientAddress = requestPacket.getAddress();
+                int clientPort = requestPacket.getPort();
 
-                // Obtenga el valor de acuerdo con la clave del mapa
-                value=maps.get(key);
-                if(null == value){
-                    value="Valores predeterminados";
-                }
-                address=packet_in.getSocketAddress();
-                // verifica las condiciones de salida
-                if("down".equals(key)){
-                    System.out.println("La entrada del cliente es: "+key);
-                    reviceData="El servidor está inactivo, por favor intente nuevamente".getBytes();
-                    packet_out=new DatagramPacket(reviceData,reviceData.length,address);
-                    socket.send(packet_out);
+                if (request.equalsIgnoreCase("down")) {
+                    System.out.println("El cliente solicitó cerrar el servidor.");
                     break;
-                }else {
-                    System.out.println("La entrada del cliente es: "+key+", el resultado traducido es "+value);
-                    // Construye los datos enviados por el servidor al cliente
-                    reviceData=("La palabra inglesa que ingresó traducida al españpl es: "+value).getBytes();
-                    // Construye packet_out para enviar datos
-                    packet_out=new DatagramPacket(reviceData,reviceData.length,address);
-                    socket.send(packet_out);
                 }
+
+                String response = wordMap.getOrDefault(request, "Palabra desconocida");
+                System.out.println("El cliente ingresó: " + request + ", la traducción es: " + response);
+
+                byte[] responseData = ("La traducción de " + request + " es: " + response).getBytes();
+                DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length, clientAddress, clientPort);
+                socket.send(responsePacket);
             }
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("===== Servidor apagado =====");
         }
-    }
-
-    // Defina el conjunto de mapas para almacenar las palabras que deben traducirse.
-    private static Map<String, String> maps = new HashMap<>();
-    // Agregar datos al mapa
-    static {
-        maps.put("dog", "perro");
-        maps.put("cat", "Gato");
-        maps.put("fish", "pez");
-        maps.put("bird", "pájaro");
-        maps.put("pig", "cerdo");
     }
 }
